a8bdc62bc15ac6f1d99bca8315f277575070a30e
AyzekUorren <2SBKaW0EGWOWucS9wowB>
2018-07-26 01:26:04 +0300

    update: index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({extended: false}))
   .use(function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
