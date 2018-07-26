985821140446e1d6f6e893b1da8041a8036154ae
AyzekUorren <2SBKaW0EGWOWucS9wowB>
2018-07-27 00:03:04 +0300

    update: index

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ express()
         } else { 
           state = true; 
           console.log("True!");
-          data = dbo.collection("Content").find({"userName":queryParams.userName});
+          data = true /*dbo.collection("Content").find({"userName":queryParams.userName})*/;
           console.log(data);
         }
         db.close();
