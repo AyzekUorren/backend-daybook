029a81a5c27f6345a4da14c6f5c8d378479de1bd
AyzekUorren <2SBKaW0EGWOWucS9wowB>
2018-08-05 15:56:39 +0300

    update: index, add: oauth module

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient
 const url = "mongodb://anatoliy:009009q@ds247670.mlab.com:47670/daybook"
 const bodyParser = require('body-parser')
+/*Requests */
+const oauth = require('./src/requests/oauth')
 
 
 express()
@@ -18,73 +20,9 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .post('/OAuth/Log', function (req, res) {
-  	let state = false;
-  	if(req.body.userName != "" || req.body.userPass != "") {
-		let queryParams = req.body;
-		MongoClient.connect(url, function(err, client) {
-			if (err) throw err;
-			const dbo = client.db("daybook");
-			let data = {};
-			dbo.collection("users").count(queryParams).then((count) => {
-				if(count == 0){
-					client.close();
-					console.log("Fail log.");
-					res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
-					res.end();
-				} else { 
-					state = true; 
-					console.log("Success log.");
-					dbo.collection("Content").count({"userName":queryParams.userName}).then((count) => {
-						console.log(count);
-						if(count == 0){
-							console.log("User does not have events data.");
-							client.close();
-							console.log("Finally result: ", state);
-							res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
-							res.end();
-						} else {
-							console.log("User get events data, successful.")
-							dbo.collection("Content").findOne({"userName":queryParams.userName}, function(err, result){
-								if(err) throw err;
-								
-								data = result.userEvents;
-								client.close();
-								console.log("Finally result: ", state);
-								res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
-								res.end();
-							});
-						}
-					  
-					});
-				}
-			});
-		})
-	}
-  })
-.post('/OAuth/registration', function (req, res) {
-  let state = true;
-  let queryParams = req.body;
-  MongoClient.connect(url, function(err, client) {
-    if (err) throw err;
-    const dbo = client.db("daybook");
-    let data = null;
-     dbo.collection("users").count({"userName": queryParams.userName}).then((count) => {
-        if(count == 0){
-          console.log("Registration success , user doesn't exist.");
-          dbo.collection("users").insert({"userName": queryParams.userName, "userPass" : queryParams.userPass});
-        } else { 
-          state = false; 
-          console.log("Failed, user already exists.");
-        }
-        client.close();
-        console.log("Finally result: ", state);
-        res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
-        res.end();
-      });
-    });
-})
-.post('/events',function(req, res){
+  .post('/OAuth/Log', oauth.log(req, res))
+  .post('/OAuth/registration', oauth.registaration(req. res))
+  .post('/events',function(req, res){
   MongoClient.connect(url, function(err, client) {
     if (err) throw err;
      let query = req.body.data;
@@ -97,7 +35,7 @@ express()
     res.json(req.body);
     res.end();
 })
-.post('/events/update',function(req, res){
+  .post('/events/update',function(req, res){
 	MongoClient.connect(url, function(err, client) {
     if (err) throw err;
      let query = req.body.data;
---
+++ src/requests/oauth.js
@@ -0,0 +1,67 @@
+export.log = function (req, res) {
+  	let state = false;
+  	if(req.body.userName != "" || req.body.userPass != "") {
+		let queryParams = req.body;
+		MongoClient.connect(url, function(err, client) {
+			if (err) throw err;
+			const dbo = client.db("daybook");
+			let data = {};
+			dbo.collection("users").count(queryParams).then((count) => {
+				if(count == 0){
+					client.close();
+					console.log("Fail log.");
+					res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
+					res.end();
+				} else { 
+					state = true; 
+					console.log("Success log.");
+					dbo.collection("Content").count({"userName":queryParams.userName}).then((count) => {
+						console.log(count);
+						if(count == 0){
+							console.log("User does not have events data.");
+							client.close();
+							console.log("Finally result: ", state);
+							res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
+							res.end();
+						} else {
+							console.log("User get events data, successful.")
+							dbo.collection("Content").findOne({"userName":queryParams.userName}, function(err, result){
+								if(err) throw err;
+								
+								data = result.userEvents;
+								client.close();
+								console.log("Finally result: ", state);
+								res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
+								res.end();
+							});
+						}
+					  
+					});
+				}
+			});
+		})
+	}
+  };
+
+  export.registaration = function (req, res) {
+  let state = true;
+  let queryParams = req.body;
+  MongoClient.connect(url, function(err, client) {
+    if (err) throw err;
+    const dbo = client.db("daybook");
+    let data = null;
+     dbo.collection("users").count({"userName": queryParams.userName}).then((count) => {
+        if(count == 0){
+          console.log("Registration success , user doesn't exist.");
+          dbo.collection("users").insert({"userName": queryParams.userName, "userPass" : queryParams.userPass});
+        } else { 
+          state = false; 
+          console.log("Failed, user already exists.");
+        }
+        client.close();
+        console.log("Finally result: ", state);
+        res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
+        res.end();
+      });
+    });
+};
