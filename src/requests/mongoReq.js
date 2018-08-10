d20826156b4502294e29b8063530ff63b0482efe
AyzekUorren <2SBKaW0EGWOWucS9wowB>
2018-08-10 15:29:10 +0300

    update: log

--- src/requests/mongoReq.js
+++ src/requests/mongoReq.js
@@ -3,60 +3,64 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 const url = `mongodb://anatoliy:009009q@ds247670.mlab.com:47670/daybook`;
 
+// exports.log = function (req, res) {
+//   	let state = false;
+//   	if(req.body.userName != "" || req.body.userPass != "") {
+// 		let queryParams = req.body;
+// 		MongoClient.connect(url, function(err, client) {
+// 			if (err) throw err;
+// 			const dbo = client.db("daybook");
+// 			let data = {};
+// 			dbo.collection("users").count(queryParams).then((count) => {
+// 				if(count == 0){
+// 					client.close();
+// 					console.log("Fail log.");
+// 					res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
+// 					res.end();
+// 				} else {
+// 					state = true;
+// 					console.log("Success log.");
+// 					dbo.collection("Content").count({"userName":queryParams.userName}).then((count) => {
+// 						console.log(count);
+// 						if(count == 0){
+// 							console.log("User does not have events data.");
+// 							client.close();
+// 							console.log("Finally result: ", state);
+// 							res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
+// 							res.end();
+// 						} else {
+// 							console.log("User get events data, successful.")
+// 							dbo.collection("Content").findOne({"userName":queryParams.userName}, function(err, result){
+// 								if(err) throw err;
+//
+// 								data = result.userEvents;
+// 								client.close();
+// 								console.log("Finally result: ", state);
+// 								res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
+// 								res.end();
+// 							});
+// 						}
+//
+// 					});
+// 				}
+// 			});
+// 		})
+// 	}
+// };
+
 exports.log = function (req, res) {
-  	let state = false;
-  	if(req.body.userName != "" || req.body.userPass != "") {
-		let queryParams = req.body;
-		MongoClient.connect(url, function(err, client) {
-			if (err) throw err;
-			const dbo = client.db("daybook");
-			let data = {};
-			dbo.collection("users").count(queryParams).then((count) => {
-				if(count == 0){
-					client.close();
-					console.log("Fail log.");
-					res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
-					res.end();
-				} else {
-					state = true;
-					console.log("Success log.");
-					dbo.collection("Content").count({"userName":queryParams.userName}).then((count) => {
-						console.log(count);
-						if(count == 0){
-							console.log("User does not have events data.");
-							client.close();
-							console.log("Finally result: ", state);
-							res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
-							res.end();
-						} else {
-							console.log("User get events data, successful.")
-							dbo.collection("Content").findOne({"userName":queryParams.userName}, function(err, result){
-								if(err) throw err;
-
-								data = result.userEvents;
-								client.close();
-								console.log("Finally result: ", state);
-								res.json({"state": state, "userName": req.body.userName, "userPass": req.body.userPass, "data": data});
-								res.end();
-							});
-						}
-
-					});
-				}
-			});
-		})
-	}
-};
-
-exports.log2 = function (req, res) {
   	if(req.body.userName != "" || req.body.userPass != "") {
 		let queryParams = req.body;
     User.findOne({userName: queryParams.userName, userPass: queryParams.userPass}, function(err, user){
       if(err) throw err;
-      if(user)
-        res.status(200).json({"state": true, "userName": queryParams.userName, "userPass": queryParams.userPass, "data": user})
-      else
-        res.status(200).json({"state": false, "userName": queryParams.userName, "userPass": queryParams.userPass, "data": user})
+      if(user) {
+        console.log("Success log.", true);
+        res.status(200).json({"state": true, "userName": queryParams.userName, "userPass": queryParams.userPass, "data": user});
+      }
+      else {
+        console.log("Fail log.", false);
+        res.status(200).json({"state": false, "userName": queryParams.userName, "userPass": queryParams.userPass, "data": user});
+      }
     });
 	};
 };
