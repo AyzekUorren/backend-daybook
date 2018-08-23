8aad2664173a20af0c4bdd3f7990e7a4a80ec608
AyzekUorren <2SBKaW0EGWOWucS9wowB>
2018-08-23 19:17:21 +0300

    update: config

--- config/env.js
+++ config/env.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   port: process.env.PORT || 5000,
-  mongourl: `mongodb://anatoliy:009009q@ds247670.mlab.com:47670/daybook`,
+  mongourl: process.env.DB_URL || `mongodb://localhost/daybook`,
 }
