51844b2cb6d8513f3e8df2d854c7db96f27e97bb
AyzekUorren <ayzek1@gmail.com>
2018-09-05 16:52:51 +0300

    update: swagger config, readme

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const config = require('./config/env')
+const fs = require("fs")
 const express = require('express')
 const path = require('path')
 const PORT = config.port;
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./api/swagger/swagger.yaml')
 /*Requests */
 const mongoReq = require('./src/requests/mongoReq')
 mongoose.connect(config.mongourl, {
@@ -12,14 +16,15 @@ mongoose.connect(config.mongourl, {
 mongoose.Promise = global.Promise
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'api')))
   .use(bodyParser.urlencoded({extended: false}))
   .use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+  next();})
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
+  .get('/api/swagger/', (req, res) => res.sendFile(path.resolve('./api/swagger/swagger.yaml')))
   .post('/api/user/log', mongoReq.log)
   .post('/api/user/registration', mongoReq.registaration)
   .post('/api/events', mongoReq.events)
